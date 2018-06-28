df5740787bb5d986ee35fc3d8cedc1b72ad1a663
Shaun Bulbrook <sbulbrook@emea.corp.jwt.com>
2018-06-28 17:31:53 +0100

    Made everything super funky

--- src/components/Stage/Stage.js
+++ src/components/Stage/Stage.js
@@ -16,6 +16,7 @@ class Stage extends React.Component {
 		this.renderer = new three.WebGLRenderer({antialias:true});
 		this.data = [];
 		this.cubes = [];
+		this.lines = [];
 
 	}
 	render() {
@@ -36,6 +37,8 @@ class Stage extends React.Component {
 
 		this.socket.on('generation', data => {
 			this.data = data.data;
+
+			this.drawLines(data);
 		});
 
 	}
@@ -47,8 +50,28 @@ class Stage extends React.Component {
 
 		//box prop
 		this.pointGeom = new three.BoxGeometry( 10, 10, 10 );
-		this.pointMat = new three.MeshBasicMaterial( {color: 0x00ff00} );
+		this.pointMat = new three.MeshBasicMaterial( {color: 0x0000ff} );
 
+		this.colors = [
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+			new three.MeshBasicMaterial({color: Math.floor(Math.random()*16777215)}),
+		]
+
+		this.lineGeom = new three.Geometry();
 
 		//position camera
 		this.controls = new OrbitControls(this.camera);
@@ -62,14 +85,31 @@ class Stage extends React.Component {
 
 		const gridHelper = new three.GridHelper(1000, 100);
 		this.scene.add(gridHelper);
+
+
+
 	}
 
-	setupControls() {
-		// TODO: Add key listeners
+	drawLines ({data}) {
+		for(var i = 0; i < data.length; i++) {
+			const lineGeom = new three.Geometry();
+			if(this.lines[i]) {
+				lineGeom.vertices = this.lines[i].geometry.vertices;
+				lineGeom.vertices.push(new three.Vector3(this.data[i].x -500, this.data[i].fitness, this.data[i].y -500));
+				this.lines[i] = new three.Line(lineGeom, this.colors[Math.floor(Math.random()*this.colors.length)]);
+				this.lines[i].name = i.toString();
+				this.scene.remove(this.scene.getObjectByName(this.lines[i].name));
+				this.scene.add(this.lines[i]);
+			} else {
+				lineGeom.vertices.push(new three.Vector3(data[i].x -500, data[i].fitness, data[i].y -500));
+				this.lines[i] = new three.Line(lineGeom, this.pointMat);
+				this.scene.add(this.lines[i]);
+			}
+		}
 	}
 
 	animate() {
-		const moveSpeed = 0.3;
+		const moveSpeed = 2;
 		// console.log(this.data.length);
 		if(this.data.length){
 			for(var i = 0; i < this.data.length; i++) {
@@ -79,7 +119,7 @@ class Stage extends React.Component {
 					this.cubes[i].position.z = this.cubes[i].position.z < this.data[i].y -500 ? this.cubes[i].position.z += moveSpeed : this.cubes[i].position.z -= moveSpeed;
 				}
 				else {
-					this.cubes.push(new three.Mesh(this.pointGeom, this.pointMat));
+					this.cubes.push(new three.Mesh(this.pointGeom, this.colors[Math.floor(Math.random()*this.colors.length)]));
 					this.scene.add(this.cubes[i]);
 					this.cubes[i].position.x = this.data[i].x -500;
 					this.cubes[i].position.y = this.data[i].fitness;
@@ -88,6 +128,15 @@ class Stage extends React.Component {
 			}
 		}
 
+
+		// lineGeom.vertices.push(
+		// 	new three.Vector3( -500, 0, 0 ),
+		// 	new three.Vector3( 0, 500, 0 ),
+		// 	new three.Vector3( 0, 0, 500 )
+		// );
+		// const line = new three.Line(this.lineGeom, this.pointMat);
+		// this.scene.add(line);
+		// line.geometry.vertices.push(new three.Vector3(0,0,0));
 
 		this.renderer.render( this.scene, this.camera );
 		window.requestAnimationFrame(this.animate.bind(this));
